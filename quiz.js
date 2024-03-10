e6e71acab6d5f45c8c16b0ce2a4d58c076be81f3
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-10 13:37:53 +0100

    update

---
+++ quiz.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        questions:"which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        questions:"which is largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: true},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]  
+    }, 
+    {
+        {
+            questions:"which is smallest continent in the world?",
+            answers: [
+                {text: "Asia", correct: false},
+                {text: "Australia", correct: true},
+                {text: "Arctic", correct: false},
+                {text: "Africa", correct: false},
+            ]  
+        }
+    }
+]
