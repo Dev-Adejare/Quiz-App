32b514c6fc989a23dd889117e598e1cc07f36bef
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-10 17:05:05 +0100

    update

--- quiz.js
+++ quiz.js
@@ -72,4 +72,18 @@ function resetState(){
     }
 }
 
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+    })
+}
 startQuiz();
