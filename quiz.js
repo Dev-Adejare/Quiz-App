e1f33c3f7f838821b6aa0adc1c5e50dad89440df
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-10 13:53:30 +0100

    update

--- quiz.js
+++ quiz.js
@@ -18,7 +18,7 @@ const questions = [
         ]  
     }, 
     {
-        {
+        
             questions:"which is smallest continent in the world?",
             answers: [
                 {text: "Asia", correct: false},
@@ -26,6 +26,34 @@ const questions = [
                 {text: "Arctic", correct: false},
                 {text: "Africa", correct: false},
             ]  
-        }
+        
     }
-]
+];
+
+const questionElement = document.getElementById("quetsion");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionsNo + ". " + currentQuestion.
+    questions;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    })
+}
