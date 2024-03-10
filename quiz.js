9f341b663d495f7392b69131ee882a08fd62d00b
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-10 14:26:06 +0100

    update

--- quiz.js
+++ quiz.js
@@ -30,8 +30,8 @@ const questions = [
     }
 ];
 
-const questionElement = document.getElementById("quetsion");
-const answerButton = document.getElementById("answer-buttons");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -45,15 +45,31 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionsNo + ". " + currentQuestion.
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
     questions;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
-    })
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
 }
+
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
